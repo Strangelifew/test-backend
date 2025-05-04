f308064934b6ec24d663f7706351a15b56355cda
Strangelifew <strangelife85@mail.ru>
2025-05-04 23:17:49 +0400

    Add author table

---
+++ src/main/java/mobi/sevenwinds/app/author/AuthorTable.kt
@@ -0,0 +1,18 @@
+package mobi.sevenwinds.app.author
+
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.IntIdTable
+
+object AuthorTable: IntIdTable("author") {
+    val fullName = text("full_name")
+    val creationDate = datetime("creation_date")
+}
+
+class AuthorEntity(id: EntityID<Int>): IntEntity(id) {
+    companion object: IntEntityClass<AuthorEntity>(AuthorTable)
+
+    var fullName by AuthorTable.fullName
+    var creationDate by AuthorTable.creationDate
+}
